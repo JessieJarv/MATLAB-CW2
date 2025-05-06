735d66ab040fd7ffc2ba105aa48baccf50c49d35
Jessica Jarvis <egyjj6@nottingham.co.uk>
2025-05-06 18:14:18 +0100

    Setting up temp_function
    Committing the temperature function to the repository.

---
+++ temp_monitor.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = temp_monitor(inputArg1,inputArg2)
+%TEMP_MONITOR Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
